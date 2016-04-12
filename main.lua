618430d55d254407ee91ca18a826f8a8f6950c18
pedrotnascimento <pedrotnascimento@gmail.com>
2016-04-12 09:03:07 -0300

    Added files via upload

---
+++ main.lua
@@ -0,0 +1,13 @@
+require T1-PES
+require display
+require acoes
+
+function main ()
+	print("1)Login")
+	print("2)Operacoes(edeposito, emprestimo, saque")
+	print("3)Display(nome, limte, saque)")
+	--scan
+	if
+
+
+end
